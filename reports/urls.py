8b0658247837e8544ec88d8fb1653b7cf885a360
eavenir <98455659+Hassanpi9@users.noreply.github.com>
2023-06-21 12:52:37 +0500

    graph added

--- reports/urls.py
+++ reports/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import IncomingFundReportView, OutgoingFundReportView, JournalVoucherReportView
+from .views import IncomingFundReportView, OutgoingFundReportView, JournalVoucherReportView, TotalCountView, TotalAmountView, MonthlyIncomingFundGraphView, AnnualIncomingFundGraphView
 urlpatterns = [
     path('incoming-fund-report/', IncomingFundReportView.as_view()),
     path('outgoing-fund-report/', OutgoingFundReportView.as_view()),
     path('journal-voucher-report/', JournalVoucherReportView.as_view()),
+    path('dashboard-counts/', TotalCountView.as_view()),
+    path('dashboard-amounts/', TotalAmountView.as_view()),
+    path('monthly-incoming-fund/', MonthlyIncomingFundGraphView.as_view()),
+    path('annual-incoming-fund/', AnnualIncomingFundGraphView.as_view())
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,7 +1,14 @@
 # views.py
 from rest_framework import generics
 from payments.models import IncomingFund, OutgoingFund, JournalVoucher
+from customer.models import Customers
+from plots.models import Plots
+from booking.models import Booking, Token
 from .serializers import IncomingFundReportSerializer, OutgoingFundReportSerializer, JournalVoucherReportSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.db.models import Count, Sum, functions
+from datetime import date, datetime, timedelta
 
 
 class IncomingFundReportView(generics.ListAPIView):
@@ -69,3 +76,184 @@ class JournalVoucherReportView(generics.ListAPIView):
             queryset = queryset.filter(date__lte=end_date)
 
         return queryset
+
+
+class TotalCountView(APIView):
+    def get(self, request):
+        project_id = request.GET.get('project_id')
+        today = date.today()
+        customer_count = Customers.objects.filter(
+            project_id=project_id).count()
+        active_plots_count = Plots.objects.filter(
+            project_id=project_id, status='active').count()
+        sold_plots_count = Plots.objects.filter(
+            project_id=project_id, status='sold').count()
+        resale_plots_count = Plots.objects.filter(project_id=project_id).annotate(
+            booking_count=Count('booking_details')).filter(booking_count__gt=1).count()
+        booking_count = Booking.objects.filter(project_id=project_id).count()
+        non_expired_tokens_count = Token.objects.filter(
+            project_id=project_id, expire_date__gte=today).count()
+
+        data = {
+            'customer_count': customer_count,
+            'active_plots_count': active_plots_count,
+            'sold_plots_count': sold_plots_count,
+            'resale_plots_count': resale_plots_count,
+            'booking_count': booking_count,
+            'non_expired_tokens_count': non_expired_tokens_count,
+        }
+
+        return Response(data)
+
+
+class TotalAmountView(APIView):
+    def get(self, request):
+        start_date = request.GET.get('start_date')
+        end_date = request.GET.get('end_date')
+        project_id = request.GET.get('project_id')
+
+        # Calculate total incoming amount
+        incoming_amount = IncomingFund.objects.filter(
+            project=project_id
+        ).aggregate(total_amount=Sum('amount'))['total_amount']
+
+        # Calculate total outgoing amount
+        outgoing_amount = OutgoingFund.objects.filter(
+            project=project_id
+        ).aggregate(total_amount=Sum('amount'))['total_amount']
+
+        # Calculate total journal voucher amount
+        journal_voucher_in_amount = JournalVoucher.objects.filter(
+            project=project_id, type='in'
+        ).aggregate(total_amount=Sum('amount'))['total_amount']
+
+        journal_voucher_out_amount = JournalVoucher.objects.filter(
+            project=project_id, type='out'
+        ).aggregate(total_amount=Sum('amount'))['total_amount']
+
+        # Prepare response
+        response_data = {
+            'incoming_amount': incoming_amount or 0,
+            'outgoing_amount': outgoing_amount or 0,
+            'journal_voucher_in_amount': journal_voucher_in_amount or 0,
+            'journal_voucher_out_amount': journal_voucher_out_amount or 0
+        }
+
+        return Response(response_data)
+
+
+class MonthlyIncomingFundGraphView(APIView):
+    def get(self, request):
+        current_date = datetime.now().date()
+        start_date = current_date.replace(day=1)
+        end_date = start_date + timedelta(days=32)
+        end_date = end_date.replace(day=1)
+
+        incoming_funds_by_day = IncomingFund.objects \
+            .filter(date__gte=start_date, date__lt=end_date) \
+            .annotate(day=functions.TruncDay('date')) \
+            .values('day') \
+            .annotate(total_amount=Sum('amount')) \
+            .order_by('day')
+
+        outgoing_funds_by_day = OutgoingFund.objects \
+            .filter(date__gte=start_date, date__lt=end_date) \
+            .annotate(day=functions.TruncDay('date')) \
+            .values('day') \
+            .annotate(total_amount=Sum('amount')) \
+            .order_by('day')
+
+        incoming_report_data = [
+            {'day': fund['day'].strftime(
+                '%Y-%m-%d'), 'total_amount': fund['total_amount'] or 0}
+            for fund in incoming_funds_by_day
+        ]
+
+        outgoing_report_data = [
+            {'day': fund['day'].strftime(
+                '%Y-%m-%d'), 'total_amount': fund['total_amount'] or 0}
+            for fund in outgoing_funds_by_day
+        ]
+
+        current_day = start_date
+        while current_day < end_date:
+            current_day_str = current_day.strftime('%Y-%m-%d')
+
+            if not any(d['day'] == current_day_str for d in incoming_report_data):
+                incoming_report_data.append(
+                    {'day': current_day_str, 'total_amount': 0})
+
+            if not any(d['day'] == current_day_str for d in outgoing_report_data):
+                outgoing_report_data.append(
+                    {'day': current_day_str, 'total_amount': 0})
+
+            current_day += timedelta(days=1)
+
+        sorted_incoming_report_data = sorted(
+            incoming_report_data, key=lambda x: x['day'])
+        sorted_outgoing_report_data = sorted(
+            outgoing_report_data, key=lambda x: x['day'])
+
+        result = {
+            'incoming_funds': sorted_incoming_report_data,
+            'outgoing_funds': sorted_outgoing_report_data
+        }
+
+        return Response(result)
+
+
+class AnnualIncomingFundGraphView(APIView):
+    def get(self, request):
+        current_year = datetime.now().year
+        start_date = datetime(current_year, 1, 1)
+        end_date = datetime(current_year + 1, 1, 1)
+
+        incoming_funds_by_month = IncomingFund.objects \
+            .filter(date__gte=start_date, date__lt=end_date) \
+            .annotate(month=functions.TruncMonth('date')) \
+            .values('month') \
+            .annotate(total_amount=Sum('amount')) \
+            .order_by('month')
+
+        outgoing_funds_by_month = OutgoingFund.objects \
+            .filter(date__gte=start_date, date__lt=end_date) \
+            .annotate(month=functions.TruncMonth('date')) \
+            .values('month') \
+            .annotate(total_amount=Sum('amount')) \
+            .order_by('month')
+
+        incoming_report_data = [
+            {'month': fund['month'].strftime(
+                '%Y-%m'), 'total_amount': fund['total_amount'] or 0}
+            for fund in incoming_funds_by_month
+        ]
+
+        outgoing_report_data = [
+            {'month': fund['month'].strftime(
+                '%Y-%m'), 'total_amount': fund['total_amount'] or 0}
+            for fund in outgoing_funds_by_month
+        ]
+
+        for month in range(1, 13):
+            current_month_str = datetime(
+                current_year, month, 1).strftime('%Y-%m')
+
+            if not any(d['month'] == current_month_str for d in incoming_report_data):
+                incoming_report_data.append(
+                    {'month': current_month_str, 'total_amount': 0})
+
+            if not any(d['month'] == current_month_str for d in outgoing_report_data):
+                outgoing_report_data.append(
+                    {'month': current_month_str, 'total_amount': 0})
+
+        sorted_incoming_report_data = sorted(
+            incoming_report_data, key=lambda x: x['month'])
+        sorted_outgoing_report_data = sorted(
+            outgoing_report_data, key=lambda x: x['month'])
+
+        result = {
+            'incoming_funds': sorted_incoming_report_data,
+            'outgoing_funds': sorted_outgoing_report_data
+        }
+
+        return Response(result)
