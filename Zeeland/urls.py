cfff0e229521d17f863344349a39f122f6c97c4a
Hassan Abdul Basit <98455659+Hassanpi9@users.noreply.github.com>
2023-10-27 17:39:21 +0500

    Update urls.py

--- Zeeland/urls.py
+++ Zeeland/urls.py
@@ -23,7 +23,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("__debug__/", include("debug_toolbar.urls")),
+    # path("__debug__/", include("debug_toolbar.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('projects.urls')),
