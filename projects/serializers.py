5b78f2b6f47e1d0f10b3e8b9de45a6ed139a2e5b
Hassan <98455659+Hassanpi9@users.noreply.github.com>
2024-03-12 14:36:15 +0500

    Update serializers.py

--- projects/serializers.py
+++ projects/serializers.py
@@ -56,3 +56,22 @@ class BalanceSheetSerializer(serializers.ModelSerializer):
             for amount_detail_data in amount_details_data:
                 BalanceSheetAmountDetails.objects.create(detail=balance_sheet_detail, **amount_detail_data)
         return balance_sheet
+
+    def update(self, instance, validated_data):
+        details_data = validated_data.pop('details')
+        instance = super().update(instance, validated_data)
+        if details_data:
+            for detail_data in details_data:
+                detail_instance = instance.details.filter(id=detail_data.get('id')).first()
+                if detail_instance:
+                    amount_details_data = detail_data.pop('amount_details')
+                    for amount_detail_data in amount_details_data:
+                        amount_detail_instance = detail_instance.amount_details.filter(id=amount_detail_data.get('id')).first()
+                        if amount_detail_instance:
+                            for key, value in amount_detail_data.items():
+                                setattr(amount_detail_instance, key, value)
+                            amount_detail_instance.save()
+                    for key, value in detail_data.items():
+                        setattr(detail_instance, key, value)
+                    detail_instance.save()
+        return instance
