fa5d5a2fc68689870d1091b26cd5f184dde4b7c9
Aurangzaib00786 <aurangzaibvirk44@gmail.com>
2024-06-11 17:54:17 +0500

    ok

--- Zeeland/urls.py
+++ Zeeland/urls.py
@@ -23,9 +23,10 @@ from rest_framework_simplejwt.views import (
 )
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("__debug__/", include("debug_toolbar.urls")),
+    # path("__debug__/", include("debug_toolbar.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include("projects.urls")),
