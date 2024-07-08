960cdec854744eed90b637236cd55bee2d597d65
Hassan <98455659+Hassanpi9@users.noreply.github.com>
2024-07-08 17:09:13 +0500

    Update serializers.py

--- booking/serializers.py
+++ booking/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
-from .models import Booking,BookingDocuments, Token,PlotResale,TokenDocuments
+from .models import Booking, BookingDocuments, Token, PlotResale, TokenDocuments
 from plots.models import Plots
 from payments.models import IncomingFund
 from customer.serializers import CustomersSerializer
 from plots.serializers import PlotsSerializer
 from django.db.models import Sum
 import datetime
+
 
 class PlotsSerializer(serializers.ModelSerializer):
     category_name = serializers.SerializerMethodField(read_only=True)
@@ -21,7 +22,7 @@ class PlotsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Plots
-        fields = '__all__'  # or specify specific fields
+        fields = "__all__"  # or specify specific fields
 
 
 class BookingDocumentsSerializer(serializers.ModelSerializer):
@@ -35,77 +36,101 @@ class BookingDocumentsSerializer(serializers.ModelSerializer):
         return validated_data
 
 
-
 class BookingSerializer(serializers.ModelSerializer):
-    customer_info = CustomersSerializer(source="customer",read_only=True)
-    dealer_name=serializers.CharField(source="dealer.name",read_only=True)
-    plot_info = PlotsSerializer(source="plot",read_only=True)
+    customer_info = CustomersSerializer(source="customer", read_only=True)
+    dealer_name = serializers.CharField(source="dealer.name", read_only=True)
+    plot_info = PlotsSerializer(source="plot", read_only=True)
     files = BookingDocumentsSerializer(many=True, required=False)
-
-
 
     class Meta:
         model = Booking
-        fields = '__all__'
-        read_only_fields = ['total_receiving_amount']
+        fields = "__all__"
+        read_only_fields = ["total_receiving_amount"]
 
     def create(self, validated_data):
         files_data = validated_data.pop("files", [])
-        project = validated_data.get('project')
-        advance_amount = validated_data.get('advance')
-        total_amount=validated_data.get("total_amount")
-        booking_date=validated_data.get("booking_date")
-        installement_month=datetime.datetime(booking_date.year, booking_date.month, 1).date()
+        project = validated_data.get("project")
+        advance_amount = validated_data.get("advance")
+        total_amount = validated_data.get("total_amount")
+        booking_date = validated_data.get("booking_date")
+        bank = validated_data.get("bank")
+        payment_type = validated_data.get("payment_type")
+        cheque_number = validated_data.get("cheque_number")
+        installement_month = datetime.datetime(
+            booking_date.year, booking_date.month, 1
+        ).date()
         try:
-            latest_booking = Booking.objects.filter(
-                project=project).latest('created_at')
-            latest_booking_number = int(
-                latest_booking.booking_id.split('-')[1]) + 1
+            latest_booking = Booking.objects.filter(project=project).latest(
+                "created_at"
+            )
+            latest_booking_number = int(latest_booking.booking_id.split("-")[1]) + 1
         except:
             latest_booking_number = 1
 
-        booking_id_str = str(project.id) + \
-            '-' + str(latest_booking_number).zfill(3)
-        validated_data['booking_id'] = booking_id_str
-        validated_data['total_receiving_amount'] = advance_amount
+        booking_id_str = str(project.id) + "-" + str(latest_booking_number).zfill(3)
+        validated_data["booking_id"] = booking_id_str
+        validated_data["total_receiving_amount"] = advance_amount
 
-        plot = validated_data['plot']
+        plot = validated_data["plot"]
         plot.status = "sold"
         plot.save()
-        existing_resale_exists = Booking.objects.filter(plot=plot, status="resale").exists()
+        existing_resale_exists = Booking.objects.filter(
+            plot=plot, status="resale"
+        ).exists()
         booking = Booking.objects.create(**validated_data)
         if booking:
-            IncomingFund.objects.create(project=project,booking=booking,date=booking_date,installement_month=installement_month,amount=advance_amount,remarks="advance",advance_payment=True)
+            IncomingFund.objects.create(
+                project=project,
+                booking=booking,
+                date=booking_date,
+                installement_month=installement_month,
+                amount=advance_amount,
+                remarks="advance",
+                advance_payment=True,
+                bank=bank,
+                payment_type=payment_type,
+                cheque_number=cheque_number,
+            )
         if existing_resale_exists:
-            PlotResale.objects.create(booking=booking,plot=plot,entry_type="booking",new_plot_price=total_amount)
+            PlotResale.objects.create(
+                booking=booking,
+                plot=plot,
+                entry_type="booking",
+                new_plot_price=total_amount,
+            )
         for file_data in files_data:
             BookingDocuments.objects.create(booking=booking, **file_data)
         return booking
 
     def update(self, instance, validated_data):
         files_data = validated_data.pop("files", [])
-        booking_status = validated_data.get('status', instance.status)
-        total_amount = validated_data.get(
-            'total_amount', instance.total_amount)
-        advance_amount = validated_data.get('advance', instance.advance)
-        advance_payment_obj = IncomingFund.objects.filter(booking=instance, advance_payment=True).first()
+        booking_status = validated_data.get("status", instance.status)
+        total_amount = validated_data.get("total_amount", instance.total_amount)
+        advance_amount = validated_data.get("advance", instance.advance)
+        advance_payment_obj = IncomingFund.objects.filter(
+            booking=instance, advance_payment=True
+        ).first()
         if advance_payment_obj:
             advance_payment_obj.amount = advance_amount
             advance_payment_obj.save()
-        payments = IncomingFund.objects.filter(
-            booking=instance.id).aggregate(Sum('amount'))['amount__sum'] or 0
+        payments = (
+            IncomingFund.objects.filter(booking=instance.id).aggregate(Sum("amount"))[
+                "amount__sum"
+            ]
+            or 0
+        )
 
         for key, value in validated_data.items():
             setattr(instance, key, value)
 
-        if booking_status == 'resale' or booking_status == 'close':
+        if booking_status == "resale" or booking_status == "close":
             plot = instance.plot
-            plot.status = 'active'
+            plot.status = "active"
             plot.save()
 
         instance.status = booking_status
-        instance.total_receiving_amount = payments 
-        instance.remaining = total_amount-payments 
+        instance.total_receiving_amount = payments
+        instance.remaining = total_amount - payments
         instance.save()
 
         for file_data in files_data:
@@ -133,7 +158,7 @@ class BookingForPaymentsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Booking
-        fields = ['id', 'booking_details']
+        fields = ["id", "booking_details"]
 
 
 class TokenDocumentsSerializer(serializers.ModelSerializer):
@@ -150,7 +175,7 @@ class TokenDocumentsSerializer(serializers.ModelSerializer):
 class TokenSerializer(serializers.ModelSerializer):
     customer_name = serializers.SerializerMethodField(read_only=True)
     plot_info = serializers.SerializerMethodField(read_only=True)
-    bank_name=serializers.CharField(source="bank.name",read_only=True)
+    bank_name = serializers.CharField(source="bank.name", read_only=True)
     files = TokenDocumentsSerializer(many=True, required=False)
 
     def get_customer_name(self, instance):
@@ -164,8 +189,7 @@ class TokenSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Token
-        fields = '__all__'
-
+        fields = "__all__"
 
     def create(self, validated_data):
         files_data = validated_data.pop("files", [])
@@ -195,23 +219,23 @@ class TokenSerializer(serializers.ModelSerializer):
         return instance
 
 
-
 class PlotResaleSerializer(serializers.ModelSerializer):
-    customer=serializers.CharField(source="booking.customer.name",read_only=True)
-    booking_number=serializers.CharField(source="booking.booking_id",read_only=True)
-    total_amount=serializers.FloatField(source="booking.total_amount",read_only=True)
-    amount_received=serializers.FloatField(source="booking.total_receiving_amount",read_only=True)
-    remaining=serializers.FloatField(source="booking.remaining",read_only=True)
+    customer = serializers.CharField(source="booking.customer.name", read_only=True)
+    booking_number = serializers.CharField(source="booking.booking_id", read_only=True)
+    total_amount = serializers.FloatField(source="booking.total_amount", read_only=True)
+    amount_received = serializers.FloatField(
+        source="booking.total_receiving_amount", read_only=True
+    )
+    remaining = serializers.FloatField(source="booking.remaining", read_only=True)
 
     plot_info = serializers.SerializerMethodField(read_only=True)
-
 
     def get_plot_info(self, instance):
         plot_number = instance.plot.plot_number
         plot_size = instance.plot.get_plot_size()
         plot_type = instance.plot.get_type_display()
         return f"{plot_number} || {plot_type} || {plot_size}"
-    
+
     class Meta:
         model = PlotResale
-        fields = '__all__'
+        fields = "__all__"
